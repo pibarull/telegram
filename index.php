416ad95b614baa133a618c022cfd282b5cd4adda
Ilya <31762469+pibarull@users.noreply.github.com>
2017-09-15 01:23:16 +0300

    Update index.php

--- index.php
+++ index.php
@@ -6,4 +6,5 @@
  * Time: 0:55
  */
 $output = json_decode(file_get_contents('php://input'), true);
-file_put_contents('logs.txt', $output );
+$id = $output['message']['chat']['id'];
+file_put_contents('logs.txt', $id );
