f9052123fd21d3c31fb70eb615211d0131de9552
Ilya <31762469+pibarull@users.noreply.github.com>
2017-09-15 01:33:11 +0300

    Update index.php

--- index.php
+++ index.php
@@ -7,4 +7,5 @@
  */
 $output = json_decode(file_get_contents('php://input'), true);
 $id = $output['message']['chat']['id'];
+file_get_contents('https://api.telegram.org/bot448065005:AAGM8sP7sKzRK24VspHbU1JoKcGzHhO4THU/sendMessage?chat_id=".$id."&text=Hi! It is my first message to you, human!"');
 file_put_contents('logs.txt', $id );
